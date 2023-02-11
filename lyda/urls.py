f8c743541660781536d0226a630c44f72daad556
athulya <athulyaathu.uk@gmail.com>
2023-02-12 01:12:25 +0530

    template add

--- lyda/urls.py
+++ lyda/urls.py
@@ -32,3 +32,5 @@ urlpatterns = [
     path('orders/', include('orders.urls')),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
